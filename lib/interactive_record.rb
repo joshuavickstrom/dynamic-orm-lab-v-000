45ad41d2fac769230aa8ef3846f9d9878223abbd
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-01 17:35:36 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,7 +48,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attributes)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attributes} =?"
+    sql = "SELECT * FROM #{self.table_name} WHERE '#{attributes}' =?"
     DB[:conn].execute(sql)
   end
 
